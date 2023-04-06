c5b21ba1f1501cd227f82902e8afe8727fca4f18
Preston Button <prestonbutton99@gmail.com>
2023-04-06 11:56:59 -0400

    configure and set up tailwindCSS in React project

--- app/diceGame/src/App.jsx
+++ app/diceGame/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./tailwind.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="App min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <div className="flex gap-4 mb-8">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo h-16 w-auto" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img
+            src={reactLogo}
+            className="logo h-16 w-auto react"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-4xl font-bold mb-8">Vite + React</h1>
+      <div className="card p-6 bg-white shadow-lg rounded">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+        >
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+        <p className="text-gray-700">
+          Edit <code className="font-mono text-sm">src/App.jsx</code> and save
+          to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-blue-900 mt-4">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
