80700efb546c6efb805cca7f08d84b11e34ca641
Preston Button <prestonbutton99@gmail.com>
2023-04-06 12:24:55 -0400

    set up react-router with routes

--- app/diceGame/src/App.jsx
+++ app/diceGame/src/App.jsx
@@ -1,42 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./tailwind.css";
+// src/App.js
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Game from "./pages/game";
+import Account from "./pages/account";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <div className="flex gap-4 mb-8">
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo h-16 w-auto" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className="logo h-16 w-auto react"
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1 className="text-4xl font-bold mb-8">Vite + React</h1>
-      <div className="card p-6 bg-white shadow-lg rounded">
-        <button
-          onClick={() => setCount((count) => count + 1)}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-        >
-          count is {count}
-        </button>
-        <p className="text-gray-700">
-          Edit <code className="font-mono text-sm">src/App.jsx</code> and save
-          to test HMR
-        </p>
-      </div>
-      <p className="text-blue-900 mt-4">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/game/:gameId" component={Game} />
+        <Route path="/account" component={Account} />
+      </Routes>
+    </Router>
   );
 }
 
