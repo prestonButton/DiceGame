d821e4afe9bdf27ee994fe7391625cd63dd62b75
Preston Button <prestonbutton99@gmail.com>
2023-04-07 13:11:54 -0400

    add login and signup pages and update routes

--- app/diceGame/src/App.jsx
+++ app/diceGame/src/App.jsx
@@ -7,6 +7,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Game from "./pages/game";
 import Account from "./pages/account";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/game/:gameId" element={<Game />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
