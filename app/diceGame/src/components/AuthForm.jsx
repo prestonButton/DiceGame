4236f3bf729ebeb8ce98048b8e343e4169024794
sreehari21 <sreeharijayaram21@gmail.com>
2023-04-13 21:54:21 +0530

    UserCard component created

--- app/diceGame/src/components/AuthForm.jsx
+++ app/diceGame/src/components/AuthForm.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import UserCard from "./UserCard";
 
 const AuthForm = ({ type }) => {
   const [email, setEmail] = useState("");
