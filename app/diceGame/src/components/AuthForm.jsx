c25764b9e6cc026babd4621074c319c9b1199023
sreehari21 <sreeharijayaram21@gmail.com>
2023-04-13 21:34:55 +0530

    UserCard component created

--- app/diceGame/src/components/AuthForm.jsx
+++ app/diceGame/src/components/AuthForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import UserCard from "./UserCard";
 
 const AuthForm = ({ type }) => {
   const [email, setEmail] = useState("");
---
+++ app/diceGame/src/components/UserCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const UserCard = ({ isFriend }) => {
+  return (
+    <div className="userCard-container ml-10 h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
+      <div className="rounded-full bg-purple-500 h-12 w-12 flex items-center justify-center">
+        H
+      </div>
+      <p className="text-lg  font-semibold ">Hugh Jackman</p>
+      {isFriend ? (
+        <button className="text-xs m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md">
+          <span className="text-xl">+</span> Add Friend
+        </button>
+      ) : (
+        <button className="text-xs m-2 py-1 px-4 bg-white text-blue-600 font-semibold rounded-md flex items-center gap-1">
+          <span className="text-xl">-</span> Remove Friend
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default UserCard;
