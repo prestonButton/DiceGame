c2fdf184f66487ec5ee97b55683a2c10a10c0131
sreehari21 <sreeharijayaram21@gmail.com>
2023-04-14 10:51:54 +0530

    UserCard component created

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserCard = ({ isFriend }) => {
   return (
-    <div className="userCard-container ml-10 h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
+    <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
       <div className="rounded-full bg-purple-500 h-12 w-12 flex items-center justify-center">
         H
       </div>
