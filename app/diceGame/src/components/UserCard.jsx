986c79922f4332c9a9315b0291e5241c23b8f869
Preston Button <prestonbutton99@gmail.com>
2023-04-14 20:44:44 -0400

    Add Name variable and use variable for card content

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+//impleent Add/Remove button functionality here
 
-const UserCard = ({ isFriend }) => {
+const UserCard = (props) => {
+  const name = 'Hugh Jackman' //this will eventually become an API call/get this data from MongoDB
   return (
     <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
       <div className="rounded-full bg-purple-500 h-12 w-12 flex items-center justify-center">
-        H
+        {name.charAt(0).toUpperCase()}
       </div>
-      <p className="text-lg  font-semibold ">Hugh Jackman</p>
-      {isFriend ? (
+      <p className="text-lg  font-semibold ">{name}</p>
+      {props.isFriend ? (
         <button className="text-xs m-2 py-1 px-4 bg-white text-blue-600 font-semibold rounded-md flex items-center gap-1">
           <span className="text-xl">➖</span> Remove Friend
         </button>
