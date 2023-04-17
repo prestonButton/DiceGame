a018be17af8a108d1ddb48cedeb3ad887edef424
Preston Button <prestonbutton99@gmail.com>
2023-04-17 00:00:57 -0400

    update index.js to include socket.io

--- api/src/index.js
+++ api/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import mongoose from "mongoose";
+import http from "http";
+import { Server } from "socket.io";
 
 import userRoutes from "./routes/userRoutes.js";
 import gameRoutes from "./routes/gameRoutes.js";
@@ -21,5 +23,22 @@ mongoose.connect(process.env.DATABASE_URL);
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes);
 
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Replace this with the URL of your front-end (e.g., "http://localhost:3000")
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  // You can add additional event listeners here if needed
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 const port = process.env.PORT || 3001;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+httpServer.listen(port, () => console.log(`Server started on port ${port}`));
