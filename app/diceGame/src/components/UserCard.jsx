9a9138b16f609ba7022b61b382b00495728fa640
Preston Button <prestonbutton99@gmail.com>
2023-04-22 16:46:21 -0400

    change card name to props

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 //impleent Add/Remove button functionality here
 
-const UserCard = () => {
-  const name = 'Hugh Jackman' //this will eventually become an API call/get this data from MongoDB
+const UserCard = (props) => { //this will eventually become an API call/get this data from MongoDB
   return (
     <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
       <div className="rounded-full bg-purple-500 h-12 w-12 flex items-center justify-center">
-        {name.charAt(0).toUpperCase()}
+        {props.name.charAt(0).toUpperCase()}
       </div>
-      <p className="text-lg  font-semibold ">{name}</p>
+      <p className="text-lg  font-semibold ">{props.name}</p>
     </div>
   );
 };
