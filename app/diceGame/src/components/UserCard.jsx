f889ce4193be24cf357c63ee5d7be0d5a28d8990
Preston Button <prestonbutton99@gmail.com>
2023-04-22 17:15:02 -0400

    Change color Icons, userCard

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-//impleent Add/Remove button functionality here
 
-const UserCard = (props) => { //this will eventually become an API call/get this data from MongoDB
+const UserCard = (props) => {
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
   return (
     <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
-      <div className="rounded-full bg-purple-500 h-12 w-12 flex items-center justify-center">
+      <div
+        className="rounded-full"
+        style={{
+          backgroundColor: randomColor,
+          height: "50px",
+          width: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+        }}
+      >
         {props.name.charAt(0).toUpperCase()}
       </div>
-      <p className="text-lg  font-semibold ">{props.name}</p>
+      <p className="text-lg font-semibold">{props.name}</p>
     </div>
   );
 };
