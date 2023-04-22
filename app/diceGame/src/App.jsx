af97f347fbcfe6f4256dfcfa373e5eb60d6109a4
Preston Button <prestonbutton99@gmail.com>
2023-04-22 16:21:32 -0400

    fix bug

--- app/diceGame/src/App.jsx
+++ app/diceGame/src/App.jsx
@@ -1,12 +1,10 @@
-// src/App.js
 
-// src/App.jsx
 
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Game from "./pages/game";
-import Account from "./pages/account";
+import Lobby from "./pages/lobby";
 import Login from './pages/login'
 import Signup from "./pages/signup";
 
@@ -16,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game/:gameId" element={<Game />} />
-        <Route path="/account" element={<Account />} />
+        <Route path="/lobby" element={<Lobby />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- app/diceGame/src/pages/lobby.jsx
+++ app/diceGame/src/pages/lobby.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import '../tailwind.css'
 
-export default function Account() {
+export default function Lobby() {
   return (
     <div>
         <div className="h-screen bg-gradient-to-br from-blue-400 via-purple-600 to-pink-500 flex items-center justify-center text-white">
