01315792c77fbe8be8e622e6c7b0320ee6f1aa44
Preston Button <prestonbutton99@gmail.com>
2023-04-22 16:09:25 -0400

    remove friend ability from user card

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 //impleent Add/Remove button functionality here
 
-const UserCard = (props) => {
+const UserCard = () => {
   const name = 'Hugh Jackman' //this will eventually become an API call/get this data from MongoDB
   return (
     <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
@@ -9,15 +9,6 @@ const UserCard = (props) => {
         {name.charAt(0).toUpperCase()}
       </div>
       <p className="text-lg  font-semibold ">{name}</p>
-      {props.isFriend ? (
-        <button className="text-xs m-2 py-1 px-4 bg-white text-blue-600 font-semibold rounded-md flex items-center gap-1">
-          <span className="text-xl">➖</span> Remove Friend
-        </button>
-      ) : (
-        <button className="text-xs m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md">
-          <span className="text-xl">➕</span> Add Friend
-        </button>
-      )}
     </div>
   );
 };
