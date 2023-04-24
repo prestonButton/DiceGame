831bda4ace0b4a24553adfe5c3f72870fc511367
Preston Button <prestonbutton99@gmail.com>
2023-04-24 14:46:43 -0400

    keep color of user icon the same throughout

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,33 +1,34 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const UserCard = (props) => {
   const { name, score = 0 } = props;
   const location = useLocation();
-  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+
+  const [color, setColor] = useState(
+    "#" + Math.floor(Math.random() * 16777215).toString(16)
+  );
 
   return (
-    <div
-      className='userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white shadow-2xl p-4 pt-8'
-    >
+    <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white shadow-2xl p-4 pt-8">
       <div
-        className='rounded-full'
+        className="rounded-full"
         style={{
-          backgroundColor: randomColor,
-          height: '50px',
-          width: '50px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: '50%',
-          border: '2px solid white',
+          backgroundColor: color,
+          height: "50px",
+          width: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          border: "2px solid white",
         }}
       >
         {name.charAt(0).toUpperCase()}
       </div>
-      <p className='text-lg font-semibold'>{name}</p>
-      {location.pathname === '/game' && (
-        <p className='score text-base'>Score: {score}</p>
+      <p className="text-lg font-semibold">{name}</p>
+      {location.pathname === "/game" && (
+        <p className="score text-base">Score: {score}</p>
       )}
     </div>
   );
