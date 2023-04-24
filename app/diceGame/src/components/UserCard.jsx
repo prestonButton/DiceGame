5a9adbb913418ff608e89d9b130eb7b610fe7680
Preston Button <prestonbutton99@gmail.com>
2023-04-24 14:28:09 -0400

    add score to user cards when in game mode

--- app/diceGame/src/components/UserCard.jsx
+++ app/diceGame/src/components/UserCard.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const UserCard = (props) => {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  const { name, score = 0 } = props;
+  const location = useLocation();
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 
   return (
-    <div className="userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white p-4 shadow-2xl pt-8">
+    <div
+      className='userCard-container h-56 w-48 rounded-lg flex flex-col items-center justify-evenly text-white shadow-2xl p-4 pt-8'
+    >
       <div
-        className="rounded-full"
+        className='rounded-full'
         style={{
           backgroundColor: randomColor,
-          height: "50px",
-          width: "50px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          borderRadius: "50%",
-          border: "2px solid white",
+          height: '50px',
+          width: '50px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '50%',
+          border: '2px solid white',
         }}
       >
-        {props.name.charAt(0).toUpperCase()}
+        {name.charAt(0).toUpperCase()}
       </div>
-      <p className="text-lg font-semibold">{props.name}</p>
+      <p className='text-lg font-semibold'>{name}</p>
+      {location.pathname === '/game' && (
+        <p className='score text-base'>Score: {score}</p>
+      )}
     </div>
   );
 };
