eea1ea07c7db8578f0fc3c901197c21cd4c9b1a1
Preston Button <prestonbutton99@gmail.com>
2023-04-24 14:09:59 -0400

    remove scoreboard, rearrange - more is needed

--- app/diceGame/src/pages/game.jsx
+++ app/diceGame/src/pages/game.jsx
@@ -7,51 +7,35 @@ const Game = () => {
 
   return (
     <div className="h-screen bg-gradient-to-br from-blue-400 via-purple-600 to-pink-500 relative">
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-wrap w-full justify-center">
-        <Dice key={0} dots={dots} />
-        <Dice key={2} dots={dots} />
-        <Dice key={3} dots={dots} />
-        <Dice key={4} dots={dots} />
-        <Dice key={5} dots={dots} />
-        <Dice key={6} dots={dots} />
-      </div>
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-4 rounded absolute top-4 right-4">
+        Leave Game
+      </button>
 
-      <div className="flex flex-wrap w-full justify-center my-8">
-        <UserCard name="Tom Brady" />
-        <UserCard name="LeBum James" />
-        <UserCard name="Lionel Messi" />
-        <UserCard name="Christiano Ronaldo" />
-        <UserCard name="Aaron Judge" />
-        <UserCard name="Connor McGregor" />
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-wrap w-full justify-around mb-8">
+      <div className="flex flex-col items-center justify-center pt-16">
+        <div className="flex flex-wrap justify-center gap-4">
           {[...Array(6)].map((_, idx) => (
-            <div
-              key={idx}
-              className="flex flex-col items-center justify-center p-4"
-            >
-              <div className="font-bold text-2xl">User {idx + 1}</div>
-              <div className="text-gray-600">Score: 0</div>
-            </div>
+            <Dice key={idx} dots={dots} />
           ))}
         </div>
 
-        <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-wrap w-full justify-center my-8">
+          <UserCard name="Tom Brady" />
+          <UserCard name="LeBum James" />
+          <UserCard name="Lionel Messi" />
+          <UserCard name="Christiano Ronaldo" />
+          <UserCard name="Aaron Judge" />
+          <UserCard name="Connor McGregor" />
+        </div>
+
+        <div className="flex items-center justify-center">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded mr-4">
             Roll Dice
           </button>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded mr-4">
             Hold Dice
           </button>
-          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-4 rounded">
-            Leave Game
-          </button>
         </div>
       </div>
-    </div>
     </div>
   );
 };
