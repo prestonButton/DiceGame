a4f99e8a46b0752651ee3fc7e237c538843080fc
Preston Button <prestonbutton99@gmail.com>
2023-04-24 23:45:57 -0400

    add button functionality to Play button on home page

--- app/diceGame/src/pages/home.jsx
+++ app/diceGame/src/pages/home.jsx
@@ -1,10 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
 import SpinningDice from "../components/SpinningDice";
+import axios from "axios";
 
 const HomePage = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
+  const [error, setError] = useState("");
+  const accessToken = cookies.access_token;
+
+  const handlePlay = async () => {
+    try {
+      const res = await axios.get(`/games`);
+      const games = res.data;
+
+      // Check if there's a non-full game available
+      const nonFullGame = games.find(
+        (game) => game.players.length < game.maxPlayers
+      );
+      if (nonFullGame) {
+        // If there's a non-full game, join it and redirect to the lobby
+        const gameId = nonFullGame._id;
+        await axios.post(
+          `/games/${gameId}/join`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        window.location.href = `/lobby/${gameId}`;
+      } else {
+        // If there's no non-full game, create a new game and redirect to the game page
+        const res = await axios.post(
+          `/games/newgame`,
+          {
+            maxPlayers: 6,
+            winningScore: 100,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        const gameId = res.data._id;
+        await axios.post(
+          `/games/${gameId}/join`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        window.location.href = `/game/${gameId}`;
+      }
+    } catch (err) {
+      setError(err.response?.data?.message || "Failed to join game.");
+    }
+  };
+
   const logout = () => {
     setCookies("access_token", "", { path: "/" });
     window.localStorage.removeItem("userID");
@@ -30,8 +90,10 @@ const HomePage = () => {
         </div>
       ) : (
         <div className="absolute top-6 right-6 text-white">
-          {/* TODO:  Add an onclick to add user to a lobby and navigate to the lobby*/}
-          <button className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md">
+          <button
+            className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+            onClick={handlePlay}
+          >
             Play
           </button>
           <button
@@ -50,6 +112,7 @@ const HomePage = () => {
           <h2 className="text-3xl lg:text-5xl text-white">
             Multiplayer Dice Game
           </h2>
+          {error && <p className="text-red-500">{error}</p>}
         </div>
         <SpinningDice />
       </div>
