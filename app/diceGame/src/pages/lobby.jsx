575620e8c6b57c6ca65a0aebfc7f8c3fb914ceea
Preston Button <prestonbutton99@gmail.com>
2023-04-25 22:46:06 -0400

    start work on leave lobby button

--- app/diceGame/src/pages/lobby.jsx
+++ app/diceGame/src/pages/lobby.jsx
@@ -4,15 +4,23 @@ import "../tailwind.css";
 import UserCard from "../components/UserCard";
 
 const Lobby = () => {
+  const handleLeaveLobby = () => {
+    //remove user from the lobby,
+    //navigate to home page
+
+    const userID = window.localStorage.getItem("userID");
+    console.log(`${userID} left lobby`)
+  };
+
   return (
     <div className="h-screen bg-gradient-to-br from-blue-400 via-purple-600 to-pink-500 flex items-center justify-center text-white">
       <div className="flex flex-col justify-center items-center h-screen">
-        <Link
-          to="/"
+        <button
+          onClick={handleLeaveLobby}
           className="absolute top-4 right-4 m-2 py-2 px-4 bg-red-500 text-white font-semibold rounded-md"
         >
           Leave Lobby
-        </Link>
+        </button>
         <div className="grid grid-cols-3 gap-4 mt-8">
           <UserCard name="Tom Brady" />
           <UserCard name="LeBum James" />
