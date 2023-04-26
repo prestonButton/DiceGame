307e914a7505e6ac5a1ac30e8ab3f6232908a21b
Preston Button <prestonbutton99@gmail.com>
2023-04-25 20:27:21 -0400

    update gameRoutes

--- api/src/routes/gameRoutes.js
+++ api/src/routes/gameRoutes.js
@@ -1,91 +1,24 @@
 import express from "express";
 import { GameModel } from "../models/game.js";
-import { rollDice } from "../gameLogic.js";
 
 const gameRouter = express.Router();
 
-// Create a new game session
-gameRouter.post("/newgame", async (req, res) => {
-  const userId = req.user._id;
-  const newGame = new GameModel({
-    players: [userId],
-    maxPlayers: 6,
-    winningScore: 100,
-    gameState: {},
-  });
+// Get game state
+gameRouter.get("/state/:gameId", async (req, res) => {
+  try {
+    const { gameId } = req.params;
 
-  await newGame.save();
+    const game = await GameModel.findById(gameId);
+    if (!game) {
+      return res.status(404).json({ message: "Game not found" });
+    }
 
-  res.json(newGame);
-});
-
-// Join an existing game session
-gameRouter.post("/:id/join", async (req, res) => {
-  const gameId = req.params.id;
-  const userId = req.user._id;
-
-  const game = await GameModel.findById(gameId);
-  if (!game) {
-    return res.status(400).json({ message: "Game not found." });
+    return res.status(200).json({ game_state: game.game_state });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error retrieving game state", error });
   }
-
-  if (game.players.length >= game.maxPlayers) {
-    return res.status(400).json({ message: "Game is full." });
-  }
-
-  game.players.push(userId);
-  await game.save();
-
-  res.json({ message: "Successfully joined game." });
-});
-
-// Leave a game session
-gameRouter.post("/:id/leave", async (req, res) => {
-  const gameId = req.params.id;
-  const userId = req.user._id;
-
-  const game = await GameModel.findById(gameId);
-  if (!game) {
-    return res.status(400).json({ message: "Game not found." });
-  }
-
-  const index = game.players.indexOf(userId);
-  if (index === -1) {
-    return res.status(400).json({ message: "User is not in game." });
-  }
-
-  game.players.splice(index, 1);
-  await game.save();
-
-  res.json({ message: "Successfully left game." });
-});
-
-// Roll the dice
-gameRouter.post("/:id/roll", async (req, res) => {
-  const gameId = req.params.id;
-  const userId = req.user._id;
-
-  const game = await GameModel.findById(gameId);
-  if (!game) {
-    return res.status(400).json({ message: "Game not found." });
-  }
-
-  const index = game.players.indexOf(userId);
-  if (index !== 0) {
-    return res.status(400).json({ message: "It's not your turn." });
-  }
-
-  const { gameState } = game;
-
-  // Roll the dice and update the game state
-  const { score, nextPlayer } = rollDice(gameState);
-  gameState.score = score;
-  gameState.currentPlayer = nextPlayer;
-
-  game.gameState = gameState;
-  const savedGame = await game.save();
-
-  res.json(savedGame);
 });
 
 export { gameRouter };
