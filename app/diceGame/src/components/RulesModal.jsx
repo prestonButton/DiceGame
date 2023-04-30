7ace61e84ce789ae30daed22c4f1a2ed7b8c12b2
Preston Button <prestonbutton99@gmail.com>
2023-04-30 18:24:31 -0400

    Add modal to home page

--- app/diceGame/src/components/RulesModal.jsx
+++ app/diceGame/src/components/RulesModal.jsx
@@ -1,38 +1,48 @@
-import React from 'react';
+import React, { useRef } from "react";
 
 function Modal({ show, onClose }) {
+  const modalRef = useRef();
+
+  const handleClickOutside = (event) => {
+    if (modalRef.current && !modalRef.current.contains(event.target)) {
+      onClose();
+    }
+  };
+
   if (!show) {
     return null;
   }
 
   return (
-    <div>
+    <div
+      className="fixed inset-0 flex items-center justify-center z-50"
+      onClick={handleClickOutside}
+    >
       {/* Overlay */}
-      <div
-        className="fixed inset-0 bg-black opacity-50 backdrop-filter backdrop-blur-lg"
-        style={{ zIndex: 50 }}
-        onClick={onClose}
-      ></div>
+      <div className="fixed inset-0 bg-black opacity-50 backdrop-filter backdrop-blur-xl z-40"></div>
 
       {/* Modal */}
       <div
-        className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg w-3/5 md:w-1/2 lg:w-2/5 xl:w-1/3"
-        style={{ zIndex: 100 }}
+        ref={modalRef}
+        className="bg-white rounded-lg w-4/5 md:w-3/5 lg:w-1/2 xl:w-2/5 shadow-lg z-50"
       >
-        {/* Header */}
-        <div className="p-4 flex justify-between items-center border-b border-gray-200">
+        <div className="relative p-4 flex justify-between items-center border-b border-gray-200">
           <h2 className="text-blue-500 font-bold text-lg">Modal Title</h2>
-          <button className="text-gray-500 hover:text-gray-800" onClick={onClose}>
-            X
+          <button
+            className="text-red-500 text-xl absolute top-2 right-2 hover:bg-gray-200 w-6 h-6 flex items-center justify-center rounded-full focus:outline-none"
+            onClick={onClose}
+          >
+            &times;
           </button>
         </div>
 
         {/* Content */}
         <div className="p-4 text-gray-800">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque maximus tellus
-            vitae velit sollicitudin, ac scelerisque diam sodales. Proin eget dictum nunc. Duis
-            euismod quam ac justo aliquet bibendum. Nulla facilisi. Sed sed ipsum ac odio pharetra
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Pellentesque maximus tellus vitae velit sollicitudin, ac scelerisque
+            diam sodales. Proin eget dictum nunc. Duis euismod quam ac justo
+            aliquet bibendum. Nulla facilisi. Sed sed ipsum ac odio pharetra
             pulvinar sit amet non nibh.
           </p>
         </div>
--- app/diceGame/src/pages/home.jsx
+++ app/diceGame/src/pages/home.jsx
@@ -9,7 +9,7 @@ import Modal from "../components/RulesModal.jsx"
 const HomePage = () => {
   const navigate = useNavigate();
 
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
 
   const [cookies, setCookies] = useCookies(["access_token"]);
   const logout = () => {
@@ -71,56 +71,74 @@ const handleCreateLobby = () => {
   });
 };
 
+  const toggleModal = () => {
+    setShowModal((prevModal) => !prevModal);
+  };
 
-  return (
-    <div className="h-screen bg-gradient-to-br from-blue-400 via-purple-600 to-pink-500 relative">
-      {/* Conditionally render the play and the signup, login, and logout buttons depending on whether or not the user is logged in */}
-      {!cookies.access_token ? (
-        <div className="absolute top-6 right-6 text-white">
-          <Link
-            to="/signup"
-            className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
-          >
-            Sign Up
-          </Link>
-          <Link
-            to="/login"
-            className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
-          >
-            Log In
-          </Link>
-        </div>
-      ) : (
-        <div className="absolute top-6 right-6 text-white">
-          {/* TODO:  Add an onclick to add user to a lobby and navigate to the lobby*/}
-          <button
-            className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
-            onClick={handleCreateLobby}
-          >
-            Play
-          </button>
-          <button
-            className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
-            onClick={logout}
-          >
-            Log Out
-          </button>
-        </div>
-      )}
-      <div className="flex flex-col items-center justify-center h-full text-center">
-        <div>
-          <h1 className="text-5xl sm:text-9xl font-bold text-white mb-4">
-            10,000
-          </h1>
-          <h2 className="text-3xl lg:text-5xl text-white">
-            Multiplayer Dice Game
-          </h2>
-        </div>
-        <SpinningDice />
+
+return (
+  <div className="h-screen bg-gradient-to-br from-blue-400 via-purple-600 to-pink-500 relative">
+    {/* Conditionally render the rules button if the user is logged in */}
+    {cookies.access_token && (
+      <div className="absolute top-6 left-6 text-white">
+        <button
+          className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+          onClick={toggleModal}
+        >
+          Rules
+        </button>
       </div>
-      {cookies.access_token && showModal && <Modal />}
+    )}
+
+    {/* Conditionally render the play and the signup, login, and logout buttons depending on whether or not the user is logged in */}
+    {!cookies.access_token ? (
+      <div className="absolute top-6 right-6 text-white">
+        <Link
+          to="/signup"
+          className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+        >
+          Sign Up
+        </Link>
+        <Link
+          to="/login"
+          className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+        >
+          Log In
+        </Link>
+      </div>
+    ) : (
+      <div className="absolute top-6 right-6 text-white">
+        <button
+          className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+          onClick={handleCreateLobby}
+        >
+          Play
+        </button>
+        <button
+          className="m-2 py-2 px-4 bg-white text-blue-600 font-semibold rounded-md"
+          onClick={logout}
+        >
+          Log Out
+        </button>
+      </div>
+    )}
+    <div className="flex flex-col items-center justify-center h-full text-center">
+      <div>
+        <h1 className="text-5xl sm:text-9xl font-bold text-white mb-4">
+          10,000
+        </h1>
+        <h2 className="text-3xl lg:text-5xl text-white">
+          Multiplayer Dice Game
+        </h2>
+      </div>
+      <SpinningDice />
     </div>
-  );
+
+    {/* Add the Modal component */}
+    <Modal show={showModal} onClose={toggleModal} />
+  </div>
+);
+
 };
 
 export default HomePage;
